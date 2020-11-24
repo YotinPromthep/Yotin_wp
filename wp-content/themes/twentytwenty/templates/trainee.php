85fc073b8eb12180b3db900d492141bf27569789
Phet <jumpjet_191@hotmail.com>
2020-11-24 18:02:28 +0700

    add

--- wp-content/themes/twentytwenty/templates/trainee.php
+++ wp-content/themes/twentytwenty/templates/trainee.php
@@ -11,6 +11,8 @@
 get_header();
 ?>
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
 
 <main id="site-content" role="main">
     <div class="swiper-container">
@@ -30,58 +32,81 @@ get_header();
         <div class="row">
             <div class="col-3">
                 <div class="shoe-img">
-                    <img src="./wp-content/themes/child-twentytwenty/assets/img/boot_03.jpg"alt="" >
+                    <a href="#"><img src="./wp-content/themes/child-twentytwenty/assets/img/boot_03.jpg"alt="" ></a>
                 </div>
                 <div class="shoe-text">
-                    <p>Sko</p>
+                    <span>Sko</span>
                 </div>               
             </div>
             <div class="col-3">
                 <div class="shoe-img">
-                    <img src="./wp-content/themes/child-twentytwenty/assets/img/boot_05.jpg"alt="" >
+                    <a href="#"><img src="./wp-content/themes/child-twentytwenty/assets/img/boot_05.jpg"alt="" ></a>
                 </div>
                 <div class="shoe-text">
-                    <p>Støvler</p>
+                    <span>Støvler</span>
                 </div>                
             </div>
             <div class="col-3">
                 <div class="shoe-img">
-                    <img src="./wp-content/themes/child-twentytwenty/assets/img/boot_07.jpg"alt="" >
+                    <a href="#"><img src="./wp-content/themes/child-twentytwenty/assets/img/boot_07.jpg"alt="" ></a>
                 </div>
                 <div class="shoe-text">
-                    <p>Gummistøver</p>
+                    <span>Gummistøver</span>
                 </div>                
             </div>
             <div class="col-3">
                 <div class="shoe-img">
-                    <img src="./wp-content/themes/child-twentytwenty/assets/img/boot_10.jpg"alt="" >
+                    <a href="#"><img src="./wp-content/themes/child-twentytwenty/assets/img/boot_10.jpg"alt="" ></a>
                 </div>
                 <div class="shoe-text">
-                    <p>Hjemmesko</p>
+                    <span>Hjemmesko</span>
                 </div>                
             </div>
         </div>
     </div>
   </div>
 
-  <div class="child-container">
+  <div id="child-img" class="child-container">
       <div class="row">
           <div class="col-6">
               <div class="child-img">
                 <img src="./wp-content/themes/child-twentytwenty/assets/img/child_02.jpg"alt="" >
               </div>
               <div class="child-text">
-                  <p>Find den rette størrelse</p>
+                  <span>Find den rette</span> <br>
+                  <span>størrelse</span>
               </div>
           </div>
           <div class="col-6">
               <div class="child-img">
-                <img src="./wp-content/themes/child-twentytwenty/assets/img/boot_04.jpg"alt="" >
+                <img src="./wp-content/themes/child-twentytwenty/assets/img/child_04.jpg"alt="" >
               </div>
               <div class="child-text">
-                  <p></p>
+                  <span>De første</span> <br>
+                  <span>skridt</span>
               </div>
           </div>
+      </div>
+  </div>
+
+  <div class="social-container" id="social">
+      <div class="row">
+          <div class="text-bundgaard">
+              <span>#bundgaard</span>
+          </div>
+          <div class="icon-social">
+              <div class="icon-facebook"><i class="fab fa-facebook"></i></div>
+              <div class="icon-mail"><i class="fab fa-mail"></i></div>
+              <div class="icon-instargram"><i class="fab fa-instargram"></i></div>
+          </div>          
+      </div>
+      <div class="row">
+        <div class="gallery">
+            <div class="col-3"><a href="#"><img src="" alt=""></a></div>
+            <div class="col-3"><a href="#"><img src="" alt=""></a></div>
+            <div class="col-3"><a href="#"><img src="" alt=""></a></div>
+            <div class="col-3"><a href="#"><img src="" alt=""></a></div>
+        </div>
       </div>
   </div>
 
