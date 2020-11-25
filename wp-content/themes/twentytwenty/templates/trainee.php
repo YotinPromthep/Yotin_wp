730b4a3f1410b7687028fcdaae08f6839f94c989
Phet <jumpjet_191@hotmail.com>
2020-11-25 18:06:38 +0700

    add

--- wp-content/themes/twentytwenty/templates/trainee.php
+++ wp-content/themes/twentytwenty/templates/trainee.php
@@ -66,30 +66,30 @@ get_header();
     </div>
   </div>
 
-  <div id="child-img" class="child-container">
-      <div class="row">
-          <div class="col-6">
-              <div class="child-img">
-                <img src="./wp-content/themes/child-twentytwenty/assets/img/child_02.jpg"alt="" >
-              </div>
-              <div class="child-text">
+    <div id="child-img" class="child-container">
+        <div class="row">
+            <div class="col-6">
+                <div class="child-img">
+                    <a href="#"><img src="./wp-content/themes/child-twentytwenty/assets/img/child_02.jpg"alt="" ></a>
+                </div>
+                <div class="child-text">
                   <span>Find den rette</span> <br>
                   <span>størrelse</span>
-              </div>
-          </div>
-          <div class="col-6">
-              <div class="child-img">
-                <img src="./wp-content/themes/child-twentytwenty/assets/img/child_04.jpg"alt="" >
-              </div>
-              <div class="child-text">
-                  <span>De første</span> <br>
-                  <span>skridt</span>
-              </div>
-          </div>
-      </div>
-  </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="child-img">
+                    <a href="#"><img src="./wp-content/themes/child-twentytwenty/assets/img/child_04.jpg"alt="" ></a>
+                </div>
+                <div class="child-text">
+                    <span>De første</span> <br>
+                    <span>skridt</span>
+                </div>
+            </div>
+        </div>
+    </div>
 
-  <div class="social-container" id="social">
+    <div class="social-container" id="social">
         <div class="row">
             <div class="text-bundgaard">
               <a href="#"><span>#bundgaard</span></a>
@@ -105,31 +105,101 @@ get_header();
             <div class="gallery">
                 <div class="col-3">
                     <a href="#">
-                        <img src="./wp-content/themes/child-twentytwenty/assets/img/buttom-img_03.jpg" alt="">
+                        <img  src="./wp-content/themes/child-twentytwenty/assets/img/buttom-img_03.jpg" alt="">
                     </a>
+                    <div class="overlay"></div>
                     <div class="social-text">
-                        <div class="com-social">
-                            
+                        <div class="heart-social">
+                           <a href=""> <i class="fa fa-heart"> 20</i></a>
+                        </div>
+                        <div class="comment-social">
+                            <a href=""><i class="fa fa-comment"> 15</i></a>
                         </div>
                     </div>
                 </div>
                 <div class="col-3">
                     <a href="#">
-                        <img src="./wp-content/themes/child-twentytwenty/assets/img/buttom-img_05.jpg" alt="">
+                        <img  src="./wp-content/themes/child-twentytwenty/assets/img/buttom-img_05.jpg" alt="">
                     </a>
+                    <div class="overlay"></div>
+                    <div class="social-text">
+                        <div class="heart-social">
+                           <a href=""> <i class="fa fa-heart"> 20</i></a>
+                        </div>
+                        <div class="comment-social">
+                            <a href=""><i class="fa fa-comment"> 15</i></a>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-3">
                     <a href="#">
-                        <img src="./wp-content/themes/child-twentytwenty/assets/img/buttom-img_07.jpg" alt="">
+                        <img  src="./wp-content/themes/child-twentytwenty/assets/img/buttom-img_07.jpg" alt="">
                     </a>
+                    <div class="overlay"></div>
+                    <div class="social-text">
+                        <div class="heart-social">
+                           <a href=""> <i class="fa fa-heart"> 20</i></a>
+                        </div>
+                        <div class="comment-social">
+                            <a href=""><i class="fa fa-comment"> 15</i></a>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-3">
-                    <a href="#"><img src="./wp-content/themes/child-twentytwenty/assets/img/buttom-img_09.jpg" alt="">
-                </a>
-            </div>
+                    <a href="#">
+                        <img  src="./wp-content/themes/child-twentytwenty/assets/img/buttom-img_09.jpg" alt="">
+                    </a>
+                    <div class="overlay"></div>
+                    <div class="social-text">
+                        <div class="heart-social">
+                           <a href=""> <i class="fa fa-heart"> 20</i></a>
+                        </div>
+                        <div class="comment-social">
+                            <a href=""><i class="fa fa-comment"> 15</i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-  </div>
+    </div>
+
+    <div id="service" class="service-container">
+        <div class="row">
+            <div class="col-4">
+                <div class="service-img">
+                    <a href="#">
+                        <img src="./wp-content/themes/child-twentytwenty/assets/img/logo-service_06.png" alt="">
+                    </a>
+                </div>
+                <div class="service-text">
+                    <span class="head">Gratis levering</span> <br>
+                    <span class="child-head">Ved ordre over 500 kr.</span>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="service-img">
+                        <a href="#">
+                            <img src="./wp-content/themes/child-twentytwenty/assets/img/logo-service_09.png" alt="">
+                        </a>
+                    </div>
+                    <div class="service-text">
+                        <span class="head">1-2 dages levering</span> <br>
+                        <span class="child-head">Lyn hurtig levering</span>
+                    </div>
+                </div>
+            <div class="col-4">
+                <div  class="service-img">
+                        <a href="#">
+                            <img src="./wp-content/themes/child-twentytwenty/assets/img/logo-service_03.png" alt="">
+                        </a>
+                    </div>
+                    <div class="service-text">
+                        <span class="head">14 dages returret</span> <br>
+                        <span class="child-head">Tilfreds eller pengene tilbage</span>
+                    </div>
+                </div>
+        </div>
+    </div>
 
 </main><!-- #site-content -->
 
