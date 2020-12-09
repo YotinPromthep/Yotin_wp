0330e38b51d9ee427fa2d9618c7d67bf2311539b
YotinPromthep <jumpjet_191@hotmail.com>
2020-12-09 15:17:34 +0700

    aa

--- wp-content/themes/child-twentytwenty/assets/js/index.js
+++ wp-content/themes/child-twentytwenty/assets/js/index.js
@@ -1,5 +1,11 @@
 
+// Input search
+
+// <<---------------------------------------------------------------------->>
+// <<---------------------------------------------------------------------->>
+
 jQuery("#main-search").click(function() {
+
     if ( jQuery("#show-search").css("display") === "none" ) {
 
         jQuery("#show-search").css("display", "block");
@@ -8,30 +14,57 @@ jQuery("#main-search").click(function() {
 
         jQuery("#show-search").css("display", "none");
 
-    }    
+    }   
+
 }); 
 
+// language
+
+// <<---------------------------------------------------------------------->>
+// <<---------------------------------------------------------------------->>
+
 jQuery("#drop a").click(function() {
+
     var t = jQuery(this).attr('id'); 
-    jQuery('#drop-show').text(t);                      
+    jQuery('#drop-show').text(t);
+
 });
 
+// Open / Cloce Nav
+
+// <<---------------------------------------------------------------------->>
+// <<---------------------------------------------------------------------->>
+
 jQuery(document).ready(function() {
+
     jQuery('.menu__icon').click(function(){
+
         jQuery('body').toggleClass('menu_shown');
+
     });
+
   });
 
 function openNav() {
+
     document.getElementById("mySidenav").style.width = "250px";
+
 }
   
 function closeNav() {
+
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
+
 }
 
+// footer Responsive
+
+// <<---------------------------------------------------------------------->>
+// <<---------------------------------------------------------------------->>
+
 jQuery(".text-head .hidd1").click(function() {
+
     if ( jQuery(".hid1").css("display") === "none" ) {
 
         jQuery(".hid1").css("display", "block");
@@ -40,9 +73,12 @@ jQuery(".text-head .hidd1").click(function() {
 
         jQuery(".hid1").css("display", "none");
 
-    }    
+    }   
+
 });
+
 jQuery(".text-head .hidd2").click(function() {
+
     if ( jQuery(".hid2").css("display") === "none" ) {
 
         jQuery(".hid2").css("display", "block");
@@ -51,14 +87,24 @@ jQuery(".text-head .hidd2").click(function() {
 
         jQuery(".hid2").css("display", "none");
 
-    }    
+    }  
+
 });
+
+// To the top
+
+// <<---------------------------------------------------------------------->>
+// <<---------------------------------------------------------------------->>
 
 jQuery("a[href='#top']").click(function() {
     jQuery("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
 
+// hover nav
+
+// <<---------------------------------------------------------------------->>
+// <<---------------------------------------------------------------------->>
 
 jQuery(".hov").hover(function(){
     if ( jQuery(".show-nav").css("display") === "none" ) {
@@ -85,11 +131,13 @@ jQuery(".show-nav li").hover(function() {
     var id_parent = $(this).attr('data-parent');
     $('.show-nav li').css('background','#fff');
     $(this).css('background',color[id_parent]);
+
 });
 
+// Toggle
 
-
-
+// <<---------------------------------------------------------------------->>
+// <<---------------------------------------------------------------------->>
 
 var showChar = 590;
 var ellipsestext = "...";
@@ -98,6 +146,7 @@ var lesstext = "Show less";
 
 
 jQuery('.more').each(function() {
+
     var content = $(this).html();
 
     if(content.length > showChar) {
@@ -133,28 +182,49 @@ jQuery(".morelink").click(function(){
     return false;
 });
 
-// jQuery(function() {
+// Aotomatic slider
 
-//     var width = 720;
-//     var speed = 800;
-//     var pause = 800;
-//     var current = 1;
+// <<---------------------------------------------------------------------->>
+// <<---------------------------------------------------------------------->>
 
-//     //cache DOM
-//     var $slider = $('.swiper-wrapper');
-//     var $slides = $slider.find('.swiper-wrapper');
-//     var $slide = $slides.find('.swiper-slide');
-
-
-// setInterval(function() {
-//         //move image the defined width and speed to the left
-//     $slides.animate({'margin-left': '-='+width}, speed, function() {
-//         //count number of slides and loop back to first from last
-//         current++;
-//         if (current === $slides.length) {
-//             current = 1;
-//             $slides.css('margin-left', 0);
-//         }
-//     });			
-// }, pause);
-// });
+// jQuery(document).ready(function ($) {
+  
+//     function do_slide(){
+//         interval = setInterval(function(){
+//           moveRight();
+//         }, 6000);
+//       }
+//       do_slide();
+      
+      
+//         var slideCount = $('.swiper-wrapper .swiper-slide').length;
+//         var slideWidth = $('.swiper-wrapper .swiper-slide').width();
+//         var slideHeight = $('.swiper-wrapper .swiper-slide').height();
+//         var sliderUlWidth = slideCount * slideWidth;
+        
+      
+//         $('.swiper-wrapper').css({ width: slideWidth, height: slideHeight });
+        
+//         $('.swiper-wrapper .swiper-slide').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+        
+//         $('.swiper-wrapper .swiper-slide img:last-child').prependTo('.swiper-wrapper .swiper-slide');
+    
+//         function moveLeft() {
+//             $('.swiper-wrapper .swiper-slide').animate({
+//                 left: + slideWidth
+//             }, 700, function () {
+//                 $('.swiper-wrapper .swiper-slide img:last-child').prependTo('.swiper-wrapper .swiper-slide');
+//                 $('.swiper-wrapper .swiper-slide').css('left', '');
+//             });
+//         };
+    
+//         function moveRight() {
+//             $('.swiper-wrapper .swiper-slide').animate({
+//                 left: - slideWidth
+//             }, 700, function () {
+//                 $('.swiper-wrapper .swiper-slide img:first-child').appendTo('.swiper-wrapper .swiper-slide');
+//                 $('.swiper-wrapper .swiper-slide').css('left', '');
+//             });
+//         };       
+// });    
+    
